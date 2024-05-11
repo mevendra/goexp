a28a04ebd92681ea51f4eeea3333a0e5116ab21a
Maev <emilio-51@hotmail.com>
2024-05-11 09:59:46 -0300

    GRPC List

--- carch/internal/infra/grpc/service/order_service.go
+++ carch/internal/infra/grpc/service/order_service.go
@@ -1,20 +1,21 @@
 package service
 
 import (
-	"context"
-
 	"carch/internal/infra/grpc/pb"
 	"carch/internal/usecase"
+	"context"
 )
 
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrderUseCase   usecase.ListOrderUseCase
 }
 
-func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase, listOrderUseCase usecase.ListOrderUseCase) *OrderService {
 	return &OrderService{
 		CreateOrderUseCase: createOrderUseCase,
+		ListOrderUseCase:   listOrderUseCase,
 	}
 }
 
@@ -34,4 +35,21 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
 	}, nil
+}
+
+func (s *OrderService) ListOrders(ctx context.Context, in *pb.Blank) (*pb.OrderListResponse, error) {
+	output, err := s.ListOrderUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+	ordersResponse := make([]*pb.Order, len(output))
+	for i, o := range output {
+		ordersResponse[i] = &pb.Order{
+			Id:         o.ID,
+			Price:      float32(o.Price),
+			Tax:        float32(o.Tax),
+			FinalPrice: float32(o.FinalPrice),
+		}
+	}
+	return &pb.OrderListResponse{Order: ordersResponse}, nil
 }
